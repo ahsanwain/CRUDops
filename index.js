8ec813b35b835cbdb3f3ef42d62c6c84258a6d2f
Ahsan Wain <ahsan.wain@bitverx.io>
2023-11-28 17:30:58 +0500

    Middle Ware (body-parser) added.

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("<h1>Welcome</h1><p>My first express app</p>");
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/register', (req, res) => {
-    res.sendStatus(201);
-});
-
-app.put('/users/ahsan', (req, res) => {
-    res.sendStatus(200);
-});
-
-app.patch('/users/ahsan', (req, res) => {
-    res.sendStatus(200);
-});
-
-app.delete('/users/ahsan', (req, res) => {
-    res.sendStatus(200);
-});
+app.post('/api/submit', (req, res) => {
+    console.log(req.body);
+    res.send('POST request to the homepage');
+    });
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
