554eee2b66f5284a89fe0835caeb22208224f297
Ahsan Wain <ahsan.wain@bitverx.io>
2023-11-28 18:38:20 +0500

    Web form which handles POST request

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/submit', (req, res) => {
-    console.log(req.body);
-    res.send('POST request to the homepage');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    });
+
+app.post('/submit', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    res.send(`<div>
+        <h1>Welcome ${username}!</h1><p>Successful POST Request.</p>
+        </div>`);
     });
 
 app.listen(3000, () => {
